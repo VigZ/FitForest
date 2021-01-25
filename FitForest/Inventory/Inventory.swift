53fc297625ad3b8a3776423a1c1ddb2ac7646151
VigZ <kyle.vigorito@gmail.com>
2021-01-25 10:46:42 -0500

    Added basic singleton for inventory. Barebones, just for testing.

---
+++ FitForest/Inventory/Inventory.swift
@@ -0,0 +1,19 @@
+//
+//  Inventory.swift
+//  FitForest
+//
+//  Created by Kyle Vigorito on 1/25/21.
+//
+
+import Foundation
+
+class Inventory {
+    
+    static let sharedInstance = Inventory()
+    
+    var points: Int = 0
+    
+    var itemHash = {}
+    
+    
+}
