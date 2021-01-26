fafa9dff292eb91ba42bb6cc711b66d5b563a497
VigZ <kyle.vigorito@gmail.com>
2021-01-26 11:10:24 -0500

    WIP for basic auto layout views and constraints

--- FitForest/MainViewController.swift
+++ FitForest/MainViewController.swift
@@ -31,32 +31,58 @@ class MainViewController: UIViewController {
         return label
     }()
     
+    let upperContainer: UIView = {
+        let upperContainer = UIView()
+        upperContainer.backgroundColor = .systemGreen
+        upperContainer.translatesAutoresizingMaskIntoConstraints = false
+        return upperContainer
+    }()
+    
+    let lowerContainer: UIView = {
+        let lowerContainer = UIView()
+        lowerContainer.backgroundColor = .systemBlue
+        lowerContainer.translatesAutoresizingMaskIntoConstraints = false
+        return lowerContainer
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         registerForNotifications()
         StepTracker.sharedInstance.startUpdating()
         
         view.backgroundColor = .white
-        view.addSubview(stepsLabel)
-        view.addSubview(pointsLabel)
-        view.addSubview(stateLabel)
+//        view.addSubview(stepsLabel)
+//        view.addSubview(pointsLabel)
+//        view.addSubview(stateLabel)
+        view.addSubview(upperContainer)
+        view.addSubview(lowerContainer)
         
         setupLayout()
     }
     
     private func setupLayout() {
         
-        stepsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        stepsLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        stepsLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        stepsLabel.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        upperContainer.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        upperContainer.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        upperContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+//        upperContainer.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         
-        pointsLabel.centerXAnchor.constraint(equalTo: stepsLabel.centerXAnchor, constant: 20).isActive = true
+        lowerContainer.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        lowerContainer.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        lowerContainer.topAnchor.constraint(equalTo: upperContainer.bottomAnchor).isActive = true
+        upperContainer.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         
-        stateLabel.topAnchor.constraint(equalTo: pointsLabel.bottomAnchor, constant: 150).isActive = true
-        stateLabel.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        stateLabel.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        stateLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+//        stepsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        stepsLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+//        stepsLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
+//        stepsLabel.heightAnchor.constraint(equalToConstant: 200).isActive = true
+//        
+//        pointsLabel.centerXAnchor.constraint(equalTo: stepsLabel.centerXAnchor, constant: 20).isActive = true
+//        
+//        stateLabel.topAnchor.constraint(equalTo: pointsLabel.bottomAnchor, constant: 150).isActive = true
+//        stateLabel.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+//        stateLabel.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+//        stateLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
     }
     
     private func registerForNotifications() {
