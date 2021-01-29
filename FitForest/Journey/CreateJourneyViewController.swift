5bcb5537a08d33ba46e73a528076fa8b46059216
VigZ <kyle.vigorito@gmail.com>
2021-01-29 13:17:44 -0500

    Setup some buttons and labels, as well as basic view setup

--- FitForest/Journey/CreateJourneyViewController.swift
+++ FitForest/Journey/CreateJourneyViewController.swift
@@ -14,13 +14,36 @@ class CreateJourneyViewController: UIViewController {
     let startButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = .systemGreen
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Start", for: .normal)
         return button
     }()
     
     let endButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = .systemRed
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("End", for: .normal)
+        button.addTarget(self, action: #selector(stopTapped), for: .touchUpInside)
         return button
+    }()
+    
+    let distanceLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Distance:"
+        return label
+    }()
+    
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Time:"
+        return label
+    }()
+    
+    let paceLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Pace:"
+        return label
     }()
     
     override func viewDidLoad() {
@@ -39,8 +62,28 @@ class CreateJourneyViewController: UIViewController {
         
     }
     
+    @objc func stopTapped() {
+        let alertController = UIAlertController(title: "End journey?",
+                                                message: "Do you wish to end your journey?",
+                                                preferredStyle: .actionSheet)
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alertController.addAction(UIAlertAction(title: "Save", style: .default) { _ in
+          self.endJourney()
+        })
+        alertController.addAction(UIAlertAction(title: "Discard", style: .destructive) { _ in
+          self.endJourney()
+//          _ = self.navigationController?.popToRootViewController(animated: true)
+        })
+            
+        present(alertController, animated: true)
+    }
+    
     private func setUpViews(){
+        startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        startButton.centerYAnchor.constraint(equalTo: endButton.topAnchor, constant: -20).isActive = true
         
+        endButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        endButton.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
     }
     
 
