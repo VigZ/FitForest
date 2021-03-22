bd9b5d2dfe04dc32b024b42ac9e52d1ce9972b8b
VigZ <kyle.vigorito@gmail.com>
2021-03-22 15:35:00 -0400

    fixing xcode phantom file

---
+++ FitForest/Journey/Views/JourneyTableView.swift
@@ -0,0 +1,20 @@
+//
+//  JourneyTableView.swift
+//  FitForest
+//
+//  Created by Kyle Vigorito on 3/22/21.
+//
+
+import UIKit
+
+class JourneyTableView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
