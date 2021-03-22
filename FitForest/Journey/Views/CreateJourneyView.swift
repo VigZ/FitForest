3a3ee943479530681102d7ffec6f90aa50db9a27
VigZ <kyle.vigorito@gmail.com>
2021-03-22 17:02:35 -0400

    WIP refactor create view controller

--- FitForest/Journey/Views/CreateJourneyView.swift
+++ FitForest/Journey/Views/CreateJourneyView.swift
@@ -6,22 +6,28 @@
 //
 
 import UIKit
+import MapKit
 
 class CreateJourneyView: UIView {
 
-    lazy var topContainer: TopContainer = {
-        let top = TopContainer()
-        top.translatesAutoresizingMaskIntoConstraints = false
-        return top
+    lazy var labelContainer: LabelContainer = {
+        let container = LabelContainer()
+        container.translatesAutoresizingMaskIntoConstraints = false
+        return container
     }()
     
-    lazy var bottomContainer: BottomContainer = {
-        let bottom = BottomContainer()
-        bottom.translatesAutoresizingMaskIntoConstraints = false
-        return bottom
+    let endButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemRed
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("End", for: .normal)
+        button.addTarget(self, action: #selector(stopTapped), for: .touchUpInside)
+        return button
     }()
     
-    var delegate:UIViewController!
+    private var mapView: MKMapView!
+    
+    weak var delegate:CreateJourneyViewController!
 
     override init(frame: CGRect) {
       super.init(frame: frame)
@@ -34,28 +40,89 @@ class CreateJourneyView: UIView {
     }
 
     private func setupView() {
-        addSubview(topContainer)
-        addSubview(bottomContainer)
+        addSubview(labelContainer)
+        addSubview(endButton)
+        setUpMap()
         backgroundColor = .white
         setupLayout()
     }
 
     private func setupLayout() {
       NSLayoutConstraint.activate([
-        topContainer.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
-        topContainer.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
-        topContainer.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
-        topContainer.heightAnchor.constraint(equalToConstant: 600),
-
-        bottomContainer.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
-        bottomContainer.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
-        bottomContainer.topAnchor.constraint(equalTo: topContainer.bottomAnchor),
-        bottomContainer.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor)
+        labelContainer.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+        labelContainer.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
+        labelContainer.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+        endButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+        endButton.centerYAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -20)
       ])
+    }
+    
+    private func setUpMap(){
+        let mapView = MKMapView()
+        let leftMargin:CGFloat = 10
+        let topMargin:CGFloat = 60
+        let mapWidth:CGFloat = self.frame.size.width-20
+        let mapHeight:CGFloat = 300
+        
+        mapView.frame = CGRect(x: leftMargin, y: topMargin, width: mapWidth, height: mapHeight)
+        
+        mapView.mapType = MKMapType.standard
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
+        
+        // Or, if needed, we can position map in the center of the view
+        mapView.center = self.center
+        self.addSubview(mapView)
+        self.mapView = mapView
+        mapView.delegate = self
+        mapView.userTrackingMode = .follow
+    }
+    
+    @objc func stopTapped() {
+        // TODO Fix memory leak that is probably here.
+        let alertController = UIAlertController(title: "End journey?",
+                                                message: "Do you wish to end your journey?",
+                                                preferredStyle: .actionSheet)
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alertController.addAction(UIAlertAction(title: "Save", style: .default) { _ in
+            self.delegate.endJourney(save: true)
+            let dvc = JourneyDetailViewController()
+            dvc.journey = self.delegate.journeyManager.builderPack.journeyWorkout
+            let presenting = self.delegate.presentingViewController
+            self.delegate.dismiss(animated: true){
+                presenting?.present(dvc, animated: true, completion: nil)
+            }
+        })
+        alertController.addAction(UIAlertAction(title: "Discard", style: .destructive) { _ in
+            self.delegate.endJourney(save: false)
+            self.delegate.dismiss(animated: true, completion: nil)
+        })
+        
+        delegate.timer?.invalidate()
+            
+        delegate.present(alertController, animated: true)
     }
 
     override class var requiresConstraintBasedLayout: Bool {
       return true
     }
 
+}
+
+
+extension CreateJourneyView: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+      
+      guard let polyline = overlay as? MKPolyline else {
+        
+        return MKOverlayRenderer(overlay: overlay)
+          
+      }
+    let renderer = MKPolylineRenderer(polyline: polyline)
+    renderer.strokeColor = UIColor.systemBlue
+    renderer.lineWidth = 5
+      
+      return renderer
+    }
 }
---
+++ FitForest/Journey/Views/LabelContainer.swift
@@ -0,0 +1,92 @@
+//
+//  LabelContainer.swift
+//  FitForest
+//
+//  Created by Kyle Vigorito on 3/22/21.
+//
+
+import UIKit
+
+class LabelContainer: UIView {
+
+    let distanceLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Distance: 0"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Time: 0"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let paceLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Pace: 0"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let stepsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Steps: 0"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let floorsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Floors Ascended: 0"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+      super.init(frame: frame)
+      setupView()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+      super.init(coder: aDecoder)
+      setupView()
+    }
+
+    private func setupView() {
+        backgroundColor = .systemGreen
+        addSubview(distanceLabel)
+        addSubview(timeLabel)
+        addSubview(paceLabel)
+        addSubview(stepsLabel)
+        addSubview(floorsLabel)
+        setupLayout()
+    }
+
+    private func setupLayout() {
+      NSLayoutConstraint.activate([
+        
+        distanceLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+        distanceLabel.centerYAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 20),
+        
+        timeLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+        timeLabel.centerYAnchor.constraint(equalTo: distanceLabel.bottomAnchor, constant: 20),
+        
+        paceLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+        paceLabel.centerYAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: 20),
+        
+        stepsLabel.leadingAnchor.constraint(equalTo:self.leadingAnchor, constant: 20),
+        stepsLabel.centerYAnchor.constraint(equalTo: paceLabel.bottomAnchor, constant: 20),
+        
+        floorsLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+        floorsLabel.centerYAnchor.constraint(equalTo: stepsLabel.bottomAnchor, constant: 20)
+      ])
+    }
+
+    override class var requiresConstraintBasedLayout: Bool {
+      return true
+    }
+
+
+}
