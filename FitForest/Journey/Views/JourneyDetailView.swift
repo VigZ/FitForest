baaa8aeeab0a6488293bbad701f8bf508959d67f
VigZ <kyle.vigorito@gmail.com>
2021-03-22 15:30:33 -0400

    added

---
+++ FitForest/Journey/Views/JourneyDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  JourneyDetailView.swift
+//  FitForest
+//
+//  Created by Kyle Vigorito on 3/22/21.
+//
+
+import UIKit
+
+class JourneyDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
