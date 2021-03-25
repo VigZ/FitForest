efcc88d69887f7ef3b57136ba004f19a87f01ffb
VigZ <kyle.vigorito@gmail.com>
2021-03-25 14:52:13 -0400

    add ranking enum

---
+++ FitForest/Journey/Objects/Ranking.swift
@@ -0,0 +1,15 @@
+//
+//  Ranking.swift
+//  FitForest
+//
+//  Created by Kyle Vigorito on 3/25/21.
+//
+
+import Foundation
+
+enum Ranking {
+    case bronze
+    case silver
+    case gold
+    case diamond
+}
