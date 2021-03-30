419c52a552a4778c67abf769373b195ef98aefc6
VigZ <kyle.vigorito@gmail.com>
2021-03-30 09:23:43 -0400

    added files for forest scene

---
+++ FitForest/Forest/ForestController.swift
@@ -0,0 +1,50 @@
+//
+//  ForestController.swift
+//  FitForest
+//
+//  Created by Kyle Vigorito on 3/30/21.
+//
+
+import Foundation
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class ForestController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "ForestScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
