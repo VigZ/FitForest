84e98c3c6585af666568883123097eba75a59055
VigZ <kyle.vigorito@gmail.com>
2021-03-30 20:19:47 -0400

    removed line

--- FitForest/Forest/ForestScene.swift
+++ FitForest/Forest/ForestScene.swift
@@ -15,7 +15,8 @@ class ForestScene: SKScene {
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
-        
+        let node = SKLabelNode(text: "This is a test")
+        self.addChild(node)
         // Get label node from scene and store it for use later
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
         if let label = self.label {
