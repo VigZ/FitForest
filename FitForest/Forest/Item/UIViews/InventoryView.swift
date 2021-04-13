258ea5173ef9ddfac073c1b10ba1bc73ea065137
VigZ <kyle.vigorito@gmail.com>
2021-04-13 16:54:33 -0400

    added file stubs for inventory views and cards

---
+++ FitForest/Forest/Item/UIViews/InventoryView.swift
@@ -0,0 +1,14 @@
+//
+//  InventoryView.swift
+//  FitForest
+//
+//  Created by Kyle Vigorito on 4/13/21.
+//
+
+import Foundation
+import SpriteKit
+import UIKit
+
+class InventoryView: UIView {
+    
+}
