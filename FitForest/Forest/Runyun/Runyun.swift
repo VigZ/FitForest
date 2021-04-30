27bd4a2dace3df6bc491668127b571d51f559df1
VigZ <kyle.vigorito@gmail.com>
2021-04-29 21:14:50 -0400

    WIP for test of Runyun observing system

--- FitForest/Forest/Runyun/Runyun.swift
+++ FitForest/Forest/Runyun/Runyun.swift
@@ -12,6 +12,7 @@ class Runyun: SKSpriteNode, Placeable {
     var isBeingMoved: Bool = false
     var state: RunyunState!
     var accessory: AccessoryNode?
+    var observedStepsRemaining: Int
     
     func pickedUp() {
         
@@ -28,6 +29,7 @@ class Runyun: SKSpriteNode, Placeable {
         let color = UIColor.clear
         let size = texture.size()
         self.state = RunyunState.seedling
+        self.observedStepsRemaining = 100
         // Call the designated initializer
         super.init(texture: texture, color: color, size: size)
 
@@ -36,14 +38,43 @@ class Runyun: SKSpriteNode, Placeable {
 //        physicsBody?.categoryBitMask = 1
 //        physicsBody?.affectedByGravity = false
         self.zPosition = 1
+        if self.state == .seedling {
+            let ns = NotificationCenter.default
+            let stepCountUpdated = Notification.Name.StepTrackerEvents.stepCountUpdated
+            
+            ns.addObserver(forName: stepCountUpdated, object: nil, queue: nil){
+                (notification) in
+                DispatchQueue.main.async {
+                    guard let steps = notification.userInfo?["steps"] as? Int else {return}
+                    self.observedStepsRemaining -= steps
+                    print("Just added \(steps) steps.")
+                    
+                    if self.observedStepsRemaining <= 0 {
+                        self.state = .idle
+                        print("A new Runyun has hatched!")
+                        ns.removeObserver(self)
+                    }
+                    
+                    
+                }
+            }
+        }
         
     }
 
     required init?(coder aDecoder: NSCoder) {
+        self.observedStepsRemaining = aDecoder.decodeInteger(forKey: "observedStepsRemaining")
         super.init(coder: aDecoder)
     }
+    
+    override func encode(with aCoder: NSCoder) {
+        super.encode(with: aCoder)
+        aCoder.encode(self.observedStepsRemaining, forKey: "observedStepsRemaining")
+    }
+
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         print("touched")
     }
 }
+
