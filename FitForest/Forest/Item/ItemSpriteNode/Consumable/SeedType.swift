95048176a3780f9ddbbb69039946846a512b6bca
VigZ <kyle.vigorito@gmail.com>
2021-05-03 12:37:07 -0400

    added seed type enum

---
+++ FitForest/Forest/Item/ItemSpriteNode/Consumable/SeedType.swift
@@ -0,0 +1,19 @@
+//
+//  SeedType.swift
+//  FitForest
+//
+//  Created by Kyle Vigorito on 5/3/21.
+//
+
+import Foundation
+
+enum SeedType: String, Codable {
+    case red
+    case blue
+    case purple
+    case pink
+    case orange
+    case green
+    case pearl
+    case charcoal
+}
