5459f0f09fe6ce7663798855266cd5fe9d3b1469
VigZ <kyle.vigorito@gmail.com>
2021-05-05 15:39:59 -0400

    added seed modifier enum and moved seed type location

---
+++ FitForest/Forest/Item/Consumable/SeedModifier.swift
@@ -0,0 +1,13 @@
+//
+//  SeedModifier.swift
+//  FitForest
+//
+//  Created by Kyle Vigorito on 5/5/21.
+//
+
+import Foundation
+
+enum SeedModifier: String, Codable {
+    case basic
+    case rare
+}
