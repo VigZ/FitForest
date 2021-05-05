10354630b5d47b5908cc9fcc2d5fea150b586e00
VigZ <kyle.vigorito@gmail.com>
2021-05-05 19:47:41 -0400

    added Runyun Storage Hotspot

---
+++ FitForest/Forest/Hotspot/RunyunStorageSpace.swift
@@ -0,0 +1,59 @@
+//
+//  RunyunStorageSpace.swift
+//  FitForest
+//
+//  Created by Kyle Vigorito on 5/5/21.
+//
+
+import Foundation
+import SpriteKit
+
+class RunyunStorageSpace: SKSpriteNode, HotSpot {
+    
+    override var isUserInteractionEnabled: Bool {
+        get {
+            return true
+        }
+        set {
+            // Can not be set.
+        }
+    }
+    
+    init(name:String) {
+        // Make a texture from an image, a color, and size
+        let texture = SKTexture(imageNamed: name)
+        let color = UIColor.clear
+        let size = texture.size()
+
+        // Call the designated initializer
+        super.init(texture: texture, color: color, size: size)
+
+        // Set physics properties
+//        physicsBody = SKPhysicsBody(circleOfRadius: size.width / 2)
+//        physicsBody?.categoryBitMask = 1
+//        physicsBody?.affectedByGravity = false
+        self.zPosition = -199
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let sceneController = self.scene?.view?.findViewController()
+        if let sceneController = sceneController as? ForestController{
+            sceneController.showRunyunStorage()
+        }
+       
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+
+
+}
+
