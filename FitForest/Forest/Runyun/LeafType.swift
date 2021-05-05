e7f410d9b647a4a6b6c08f1a76be2940aced8e67
VigZ <kyle.vigorito@gmail.com>
2021-05-05 16:38:14 -0400

    added leaf type enum and added RSO file

---
+++ FitForest/Forest/Runyun/LeafType.swift
@@ -0,0 +1,14 @@
+//
+//  LeafType.swift
+//  FitForest
+//
+//  Created by Kyle Vigorito on 5/5/21.
+//
+
+import Foundation
+
+enum LeafType: String, Codable {
+    case standard
+    case cherry
+    case cactus
+}
---
+++ FitForest/Forest/Runyun/RunyunStorageObject.swift
@@ -0,0 +1,28 @@
+//
+//  RunyunStorageObject.swift
+//  FitForest
+//
+//  Created by Kyle Vigorito on 5/5/21.
+//
+
+import Foundation
+
+//class RunyunStorageObject: NSObject, NSCoding {
+//    
+//    var name: String
+//    var locationState: ItemState
+//    var accessory: Accessory?
+//    var observedStepsRemaining: Int
+//    var seedType: SeedType
+//
+//    
+//    func encode(with coder: NSCoder) {
+//        <#code#>
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        <#code#>
+//    }
+//    
+//    
+//}
