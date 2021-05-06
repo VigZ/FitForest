48f7186403ea1e8eff30d71b7013115feb3ae70a
VigZ <kyle.vigorito@gmail.com>
2021-05-05 22:39:43 -0400

    creating RSO in create from seed function

--- FitForest/Forest/Runyun/RunyunNodeFactory.swift
+++ FitForest/Forest/Runyun/RunyunNodeFactory.swift
@@ -16,6 +16,8 @@ class RunyunNodeFactory {
     }
     
     func createFromSeed(seedType: SeedType) -> Runyun? {
+        let newRSO = RunyunStorageObject(name: "", locationState: .inForest, accessory: nil, observedStepsRemaining: 100, seedType: seedType, leafType: .standard)
+        GameData.sharedInstance.inventory.addRunyun(runyun: newRSO)
         let newRunyun = Runyun()
         newRunyun.size = CGSize(width: 275, height: 200)
         return newRunyun
