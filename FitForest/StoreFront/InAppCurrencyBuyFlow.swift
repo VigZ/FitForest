c5522fa566b3f2d9121f85dc8b5b4eafb8ee21e5
VigZ <kyle.vigorito@gmail.com>
2021-08-30 15:50:54 -0400

    added checks for item price and item capacity

--- FitForest/StoreFront/InAppCurrencyBuyFlow.swift
+++ FitForest/StoreFront/InAppCurrencyBuyFlow.swift
@@ -15,12 +15,43 @@ class InAppCurrencyBuyFlow: BuyFlow {
     }
     
     func validatePurchase(_ item: StoreItem) {
-        <#code#>
+        // Check to see if user has enough gems.
+        guard hasEnoughCurrency(item) else {
+            // Raise not enough currency error. Etc. Etc.
+            return
+        }
+        
+        
+        // Check to see if user has max of item in inventory.
+        
+        
     }
     
     func commitPurchase(_ item: StoreItem) {
         <#code#>
     }
     
+    private func hasEnoughCurrency(_ item: StoreItem) -> Bool{
+        let points = GameData.sharedInstance.points
+        guard points >= item.price else {
+            return false
+        }
+        return true
+    }
     
-}
+    private func hasInventorySpace(_ item: StoreItem) -> Bool {
+        let items = GameData.sharedInstance.inventory.items
+        // Loop through inventory, checking count of particular item.
+        var count = 0
+        
+        for inventoryItem in items {
+            guard count < inventoryItem.stackLimit else {
+                return false
+            }
+            if inventoryItem.name == item.name {
+                count += 1
+            }
+        }
+            return true
+        }
+    }
